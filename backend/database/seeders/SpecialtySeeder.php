359c87b6844b1b810376364303419b96f9591a24
Moussa Dione <158748506+Dionecena@users.noreply.github.com>
2026-03-10 18:23:36 +0100

    fix: remove emojis from specialty icons, use text labels instead

--- backend/database/seeders/SpecialtySeeder.php
+++ backend/database/seeders/SpecialtySeeder.php
@@ -9,23 +9,33 @@ class SpecialtySeeder extends Seeder
 {
     public function run(): void
     {
+        // Nettoyer les emojis des specialites existantes
+        Specialty::all()->each(function ($s) {
+            if ($s->icon && preg_match('/[\x{1F000}-\x{1FFFF}|\x{2600}-\x{27BF}|\x{FE00}-\x{FEFF}]/u', $s->icon)) {
+                $s->update(['icon' => null]);
+            }
+        });
+
         $specialties = [
-            ['name' => 'Médecine Générale', 'description' => 'Consultations générales, bilans de santé, suivi médical courant', 'icon' => '🏥'],
-            ['name' => 'Cardiologie', 'description' => 'Maladies du cœur et des vaisseaux sanguins', 'icon' => '❤️'],
-            ['name' => 'Pédiatrie', 'description' => 'Soins médicaux pour les enfants et adolescents', 'icon' => '👶'],
-            ['name' => 'Gynécologie', 'description' => 'Santé reproductive et suivi de grossesse', 'icon' => '🤰'],
-            ['name' => 'Dermatologie', 'description' => 'Maladies de la peau, des cheveux et des ongles', 'icon' => '🧴'],
-            ['name' => 'Ophtalmologie', 'description' => 'Maladies et troubles de la vision', 'icon' => '👁️'],
-            ['name' => 'ORL', 'description' => 'Oto-rhino-laryngologie : oreilles, nez, gorge', 'icon' => '👂'],
-            ['name' => 'Neurologie', 'description' => 'Maladies du système nerveux', 'icon' => '🧠'],
-            ['name' => 'Pneumologie', 'description' => 'Maladies des poumons et des voies respiratoires', 'icon' => '🫁'],
-            ['name' => 'Orthopédie', 'description' => 'Maladies des os, articulations et muscles', 'icon' => '🦴'],
-            ['name' => 'Stomatologie', 'description' => 'Soins dentaires et chirurgie buccale', 'icon' => '🦷'],
-            ['name' => 'Urologie', 'description' => 'Maladies du système urinaire', 'icon' => '🔬'],
+            ['name' => 'Medecine Generale', 'description' => 'Consultations generales, bilans de sante, suivi medical courant', 'icon' => 'stethoscope'],
+            ['name' => 'Cardiologie', 'description' => 'Maladies du coeur et des vaisseaux sanguins', 'icon' => 'heart-pulse'],
+            ['name' => 'Pediatrie', 'description' => 'Soins medicaux pour les enfants et adolescents', 'icon' => 'baby'],
+            ['name' => 'Gynecologie', 'description' => 'Sante reproductive et suivi de grossesse', 'icon' => 'person-pregnant'],
+            ['name' => 'Dermatologie', 'description' => 'Maladies de la peau, des cheveux et des ongles', 'icon' => 'hand-dots'],
+            ['name' => 'Ophtalmologie', 'description' => 'Maladies et troubles de la vision', 'icon' => 'eye'],
+            ['name' => 'ORL', 'description' => 'Oto-rhino-laryngologie : oreilles, nez, gorge', 'icon' => 'ear-listen'],
+            ['name' => 'Neurologie', 'description' => 'Maladies du systeme nerveux', 'icon' => 'brain'],
+            ['name' => 'Pneumologie', 'description' => 'Maladies des poumons et des voies respiratoires', 'icon' => 'lungs'],
+            ['name' => 'Orthopedie', 'description' => 'Maladies des os, articulations et muscles', 'icon' => 'bone'],
+            ['name' => 'Stomatologie', 'description' => 'Soins dentaires et chirurgie buccale', 'icon' => 'tooth'],
+            ['name' => 'Urologie', 'description' => 'Maladies du systeme urinaire', 'icon' => 'microscope'],
         ];
 
         foreach ($specialties as $specialty) {
-            Specialty::create($specialty);
+            Specialty::updateOrCreate(
+                ['name' => $specialty['name']],
+                $specialty
+            );
         }
     }
 }
